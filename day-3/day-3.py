3876878f7ed30b65571da937675229dab04aaf68
David Ganey <dhganey@gmail.com>
2016-12-04 16:04:56 -0800

    Day3 part2

--- day-3/day-3.py
+++ day-3/day-3.py
@@ -1,14 +1,34 @@
+def triangleValid(part1, part2, part3):
+    side1 = int(part1)
+    side2 = int(part2)
+    side3 = int(part3)
+    return (side1 + side2 > side3 and
+            side2 + side3 > side1 and
+            side3 + side1 > side2)
+
+# part 1
 with open('input.txt') as f:
     content = f.readlines()
     possibleTriangles = 0
     for triangle in content:
         parts = triangle.split();
-        side1 = int(parts[0])
-        side2 = int(parts[1])
-        side3 = int(parts[2])
 
-        if (side1 + side2 > side3 and
-            side2 + side3 > side1 and
-            side3 + side1 > side2):
+        if triangleValid(parts[0], parts[1], parts[2]):
             possibleTriangles = possibleTriangles + 1
+    print(possibleTriangles)
+
+#part 2
+with open('input.txt') as f:
+    content = f.readlines()
+    possibleTriangles = 0
+    pointer = 0
+    for pointer in range(0, len(content), 3):
+        row1 = content[pointer].split()
+        row2 = content[pointer + 1].split()
+        row3 = content[pointer + 2].split()
+ 
+        for i in range(0, 3):
+            if triangleValid(row1[i], row2[i], row3[i]):
+                possibleTriangles = possibleTriangles + 1
+
     print(possibleTriangles)
