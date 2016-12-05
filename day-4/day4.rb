9c23c155a260c89f7b61cd71a705273379441c3d
David Ganey <dhganey@gmail.com>
2016-12-04 22:22:35 -0800

    part 2 solution

--- day-4/day4.rb
+++ day-4/day4.rb
@@ -37,4 +37,26 @@ def get_valid_rooms
     puts sector_id_sum
 end
 
-get_valid_rooms
+# from http://stackoverflow.com/questions/31489800/rotating-letters-in-a-string-so-that-each-letter-is-shifted-to-another-letter-by
+def shift(str, n)
+  abc = ("a".."z").to_a.join
+  abc_rot = abc.chars.rotate(n).join
+  str.tr(abc, abc_rot)
+end
+
+def translate_room(room)
+    match = room.match(/(.*)-(\d*)\[(.*)\]/)
+    line_name = match[1]
+    sector_id = match[2]
+    return shift(line_name, sector_id.to_i) + " has ID " + sector_id
+end
+
+def translate_rooms
+    fileObj = File.new('input.txt', "r")
+    while (room = fileObj.gets)
+        puts translate_room(room)
+    end
+    fileObj.close
+end
+
+translate_rooms
