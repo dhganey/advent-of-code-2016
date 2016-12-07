7b7b31abff675c3c4eaacdebd4e671b380ee7f9f
David Ganey <dhganey@gmail.com>
2016-12-06 22:17:23 -0800

    Day 7 part 2

--- day-7/day7.py
+++ day-7/day7.py
@@ -5,22 +5,51 @@ def isSegmentABBA(segment):
             return True
     return False
 
+def getPossibleABAsOrBABs(segment):
+    babs = []
+    for i in range(0, len(segment) - 3 + 1):
+        window = segment[i:i+3]
+        if window[0] == window[2] and window[0] != window[1]:
+            babs.append(window)
+    return babs
+
+def generateBab(aba): # "aba" returns "bab"
+    return aba[1] + aba[0] + aba[1]
+
+def lineSupportsSSL(abas, babs):
+    for aba in abas:
+        if generateBab(aba) in babs:
+            return True
+
 with open('input.txt') as f:
     content = f.readlines()
-    validAddresses = 0
+    validTLSAddresses = 0
+    validSSLAddresses = 0
 
     for line in content:
-        lineGood = False
+        lineSupportsTLS = False
+        lineSupportsTLSOverride = False
         segments = line.replace(']', '[').split('[')
+        babs = []
+        abas = []
         for i in range(0, len(segments)):
             currentSegment = segments[i]
             isBracketSection = (i % 2 != 0)
             if isSegmentABBA(currentSegment):
                 if isBracketSection:
-                    lineGood = False
-                    break
-                else:
-                    lineGood = True
-        if lineGood:
-            validAddresses = validAddresses + 1
-    print validAddresses
+                    lineSupportsTLS = False
+                    lineSupportsTLSOverride = True
+                elif not lineSupportsTLSOverride:
+                    lineSupportsTLS = True
+
+            possibleABAsOrBABs = getPossibleABAsOrBABs(currentSegment)
+            if isBracketSection:
+                babs = babs + possibleABAsOrBABs
+            else:
+                abas = abas + possibleABAsOrBABs
+        if lineSupportsTLS:
+            validTLSAddresses = validTLSAddresses + 1
+        if lineSupportsSSL(abas, babs):
+            validSSLAddresses = validSSLAddresses + 1
+    print validTLSAddresses
+    print validSSLAddresses
